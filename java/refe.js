eb1838949adf6f5cd42bf9257b777b76bfe1c2fc
sandra200210 <ssc11413@gmail.com>
2022-11-30 13:32:16 +0100

    set

--- java/refe.js
+++ java/refe.js
@@ -4,6 +4,7 @@ function iniciar() {
     enlaces=document.querySelectorAll("a");
     for (let i = 0; i < enlaces.length; i++) {
         enlaces[i].addEventListener("click",()=>{
+            window.open(enlaces[i].getAttribute("href"));
             
         })
     }
