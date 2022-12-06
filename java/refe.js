89191e944e1b18ad0a870d7a7766e78d70179a28
sandra <ssc11413@gmail.com>
2022-12-07 00:09:52 +0100

    no se, footer  y retoques

--- java/refe.js
+++ java/refe.js
@@ -4,8 +4,11 @@ function iniciar() {
     enlaces=document.querySelectorAll("a");
     for (let i = 0; i < enlaces.length; i++) {
         enlaces[i].addEventListener("click",()=>{
-            window.open(enlaces[i].getAttribute("href"));
-            window.history.back();
+            if (enlaces[i].getAttribute("href")=="index.html") {
+                console.log("hecho");
+            }else{
+                window.open(enlaces[i].getAttribute("href"));
+            }
         })
     }
 
