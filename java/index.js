c9c046246f28c9449ee5d68f21575eb5b479a83a
xXcarlos029Xx <100120053+xXcarlos029Xx@users.noreply.github.com>
2022-12-08 12:24:06 +0100

    todo actualizado

---
+++ java/index.js
@@ -0,0 +1,1 @@
+let moneda=0;
--- java/movimiento.js
+++ java/movimiento.js
@@ -1,39 +1,64 @@
+var moneda=window.parent.document.getElementById("marcador").innerHTML;
 document.onkeydown = mover;
 function mover(e){
+
     let personaje=document.getElementById("caja");
     let der=personaje.parentElement.nextElementSibling;
     let iz=personaje.parentElement.previousElementSibling;
     let arr=document.getElementById(personaje.parentElement.id-15);
     let baj=document.getElementById(personaje.parentElement.id*15/15+15);
     e = e || window.event;
-    console.log(e.keyCode);
-    if(e.keyCode == '38'){
+
+    if(e.keyCode == '38' || e.keyCode == '87'){
         // arriba
-        document.getElementById("persona").src="../IMG/Personaje/prota-arriba.gif"
+        document.getElementById("persona").src="../IMG/Personaje/prota-arriba.gif";
         let clases = arr.classList;
         let tiene = false;
         for(let i = 0; i < clases.length; i++) {
-            if(clases[i] == 'suelo' || clases[i] == 'entrada1')
+            if(clases[i] == 'suelo' || clases[i] == 'entrada1' || clases[i] == 'money')
                 tiene = true;
+            if(clases[i] == 'money'){
+                moneda++;
+                arr.classList.remove('money');
+                window.parent.document.getElementById("marcador").innerHTML=moneda;
+            }
+            if(clases[i] == 'entrada1'){
+                window.parent.document.getElementById("frame").src="Mapa2 copy.html";
+            }
+            if(clases[i] == 'salida'){
+                window.parent.document.getElementById("frame").remove();
+                window.parent.document.getElementsByTagName("article")[0].style.backgroundImage="url('../IMG/Entrada.png')";
+                //Quitar el iframe y poner una imagen con el gif dando vueltas;
+            }
         }
         if(tiene){
-        arr.appendChild(personaje);
-        der=personaje.parentElement.nextElementSibling;
-        iz=personaje.parentElement.previousElementSibling;
-        arr=document.getElementById(personaje.parentElement.id-15);
-        baj=document.getElementById(personaje.parentElement.id*15/15+15)
+            arr.innerHTML="";
+            arr.appendChild(personaje);
+            der=personaje.parentElement.nextElementSibling;
+            iz=personaje.parentElement.previousElementSibling;
+            arr=document.getElementById(personaje.parentElement.id-15);
+            baj=document.getElementById(personaje.parentElement.id*15/15+15)
         }
     }
-    else if (e.keyCode =='40'){
+    else if (e.keyCode =='40' || e.keyCode == '83'){
         // abajo
-        document.getElementById("persona").src="../IMG/Personaje/prota-abajo.gif"
+        document.getElementById("persona").src="../IMG/Personaje/prota-abajo.gif";
         let clases = baj.classList;
         let tiene = false;
         for(let i = 0; i < clases.length; i++) {
-            if(clases[i] == 'suelo' || clases[i] == 'entrada1')
+            if(clases[i] == 'suelo' || clases[i] == 'entrada1' || clases[i] == 'money')
                 tiene = true;
+            if(clases[i] == 'money'){
+                moneda++;
+                baj.classList.remove('money');
+                window.parent.document.getElementById("marcador").innerHTML=moneda;
+            }
+            if(clases[i] == 'entrada1'){
+                window.parent.document.getElementById("frame").src="Mapa2 copy.html";
+            }
         }
         if(tiene){
+            baj.innerHTML="";
         baj.appendChild(personaje);
         der=personaje.parentElement.nextElementSibling;
         iz=personaje.parentElement.previousElementSibling;
@@ -41,16 +66,25 @@ function mover(e){
         baj=document.getElementById(personaje.parentElement.id*15/15+15);
         }
     }
-    else if (e.keyCode == '37'){
+    else if (e.keyCode == '37' || e.keyCode == '65'){
         // izquierda
-        document.getElementById("persona").src="../IMG/Personaje/prota-izq.gif"
+        document.getElementById("persona").src="../IMG/Personaje/prota-izq.gif";
         let clases = iz.classList;
         let tiene = false;
         for(let i = 0; i < clases.length; i++) {
-            if(clases[i] == 'suelo' || clases[i] == 'entrada1')
+            if(clases[i] == 'suelo' || clases[i] == 'entrada1' || clases[i] == 'money')
                 tiene = true;
+            if(clases[i] == 'money'){
+                moneda++;
+                iz.classList.remove('money');
+                window.parent.document.getElementById("marcador").innerHTML=moneda;
+            }
+            if(clases[i] == 'entrada1'){
+                window.parent.document.getElementById("frame").src="Mapa2 copy.html";
+            }
         }
         if(tiene){
+            iz.innerHTML="";
         iz.appendChild(personaje);
         der=personaje.parentElement.nextElementSibling;
         iz=personaje.parentElement.previousElementSibling;
@@ -58,16 +92,25 @@ function mover(e){
         baj=document.getElementById(personaje.parentElement.id*15/15+15);
         }
     }
-    else if (e.keyCode == '39'){
+    else if (e.keyCode == '39' || e.keyCode == '68'){
         // derecha
-        document.getElementById("persona").src="../IMG/Personaje/prota-dech.gif"
+        document.getElementById("persona").src="../IMG/Personaje/prota-dech.gif";
         let clases = der.classList;
         let tiene = false;
         for(let i = 0; i < clases.length; i++) {
-            if(clases[i] == 'suelo' || clases[i] == 'entrada1')
+            if(clases[i] == 'suelo' || clases[i] == 'entrada1' || clases[i] == 'money')
                 tiene = true;
+            if(clases[i] == 'money'){
+                moneda++;
+                der.classList.remove('money');
+                window.parent.document.getElementById("marcador").innerHTML=moneda;
+            }
+            if(clases[i] == 'entrada1'){
+                window.parent.document.getElementById("frame").src="Mapa2 copy.html";
+            }
         }
         if(tiene){
+            der.innerHTML="";
         der.appendChild(personaje);
         der=personaje.parentElement.nextElementSibling;
         iz=personaje.parentElement.previousElementSibling;
