409f1d56b410561499d8b15b7ee30e771397ec38
sandra200210 <ssc11413@gmail.com>
2022-12-09 14:22:05 +0100

    funcionaaa login a medias

--- java/login.js
+++ java/login.js
@@ -1,2 +1,13 @@
-let nombre=document.getElementById("nom").innerHTML;
-let apodo=document.getElementById("apo").innerHTML;
+
+window.addEventListener("load",iniciar);
+function iniciar() {
+    let nombre=document.getElementById("nom").innerHTML;
+    let apodo=document.getElementById("apo").innerHTML;
+    let empieza=document.getElementById("empieza");
+    let padre=document.getElementById("login");
+    padre.style.display="block";
+    empieza.addEventListener("click", ()=>{
+        
+        padre.style.display="none";
+    })
+}
