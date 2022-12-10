8feffaf12de25de187b52ab881eaf640a72b1210
sandra <ssc11413@gmail.com>
2022-12-10 14:40:46 +0100

    dtykudtykudtyk

--- java/login.js
+++ java/login.js
@@ -1,6 +1,3 @@
-
-let padre=document.getElementById("login");
-let fondo=document.getElementById("hi");
 let empieza=document.getElementById("empieza");
 /* window.addEventListener("load",iniciar);
 function iniciar() {
@@ -8,7 +5,8 @@ function iniciar() {
 function empezar() {
     nom=document.getElementById("nom").value;
     apo=document.getElementById("apo").value;
-    localStorage.setItem(nom,apo);
+    localStorage.setItem("nombre",nom);
+    localStorage.setItem("apodo",apo);
     document.getElementById("login").style.display="none";
     document.getElementById("hi").style.display="none";
 }
