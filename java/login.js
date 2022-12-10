da977093e4a2136eb3f001c56fe384027d276a0a
sandra <ssc11413@gmail.com>
2022-12-10 14:33:30 +0100

    funciona login

--- java/login.js
+++ java/login.js
@@ -1,7 +1,8 @@
+
 let padre=document.getElementById("login");
 let fondo=document.getElementById("hi");
-let empieza=document.getElementById("empieza");/* 
-window.addEventListener("load",iniciar);
+let empieza=document.getElementById("empieza");
+/* window.addEventListener("load",iniciar);
 function iniciar() {
 } */
 function empezar() {
