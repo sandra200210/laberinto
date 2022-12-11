5b26d2df4c9dd43fa4b2e357dfa5b8b8f9e4ed7a
sandra <ssc11413@gmail.com>
2022-12-11 23:01:48 +0100

    mas cambios mas

--- java/index.js
+++ java/index.js
@@ -4,20 +4,31 @@ let apodo="";
 let tiempo=0;
 let monedita="";
 
+/* window.addEventListener("load",iniciar); */
 window.addEventListener("load",iniciar);
-
 function iniciar() {
     if (localStorage.getItem("RK")==null) {
         localStorage.setItem("RK","");
     }
-    leerRK();/* si lo escribimos directamente aqui da 2 fallos:
+    leerRK();
+
+    /* document.getElementById("final").addEventListener("load",leerRK); */
+
+    /* if (document.getElementById("final").style.display=="block") {
+        document.querySelectorAll("p")[1].addEventListener("load",leerRK);
+    } */
+    /* si lo escribimos directamente aqui da 2 fallos:
     - O se queda en nulo y no añade el jugador
     - o te repite varias veces el jugador que supongo que sera por lo de que se recarga sola la pagina */
 }
+if (document.getElementById("final").style.display=="block") {
+    
+    leerRK();
+} 
 
 function fin() {
-    monedita=document.getElementById("marcador").innerHTML;
     const rank=localStorage.getItem("RK").split("*");
+    monedita=document.getElementById("marcador").innerHTML;
     const jugador=[localStorage.getItem("apodo"),monedita,tiempo];
     let ranki=jugador.join(";");
     rank.push(ranki);
@@ -32,8 +43,14 @@ function fin() {
 function leerRK(){
     fin();
     const rank=localStorage.getItem("RK").split("*");
-    let padre=document.getElementById("lista");
-    
+    let padre=document.getElementById("tabla");
+    /* debe de limpiarse todo lo que es en la tabla antes de volver a cargar los datos */
+    if (padre.children!=0) {
+        const elim=padre.children;
+        for (let i = 0; i < elim.length; i++) {
+            padre.removeChild(elim[i]);
+        }
+    }
     for (let i = 0; i < rank.length; i++) {
         let caja= document.createElement("tr");
         const datos_jugador=rank[i].split(";");/* es el array de jugador pero con diferente nombre */
